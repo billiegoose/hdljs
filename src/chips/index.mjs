43b480bf99202056794d0076e57136cdaee9db9f
William Hilton <wmhilton@gmail.com>
2019-03-05 22:55:43 -0500

    start on Memory

--- src/chips/index.mjs
+++ src/chips/index.mjs
@@ -17,11 +17,13 @@ export * from './Mux16.mjs';
 export * from './DMux8Way.mjs';
 export * from './Mux4Way16.mjs';
 export * from './Mux8Way16.mjs';
+
 export * from './HalfAdder.mjs';
 export * from './FullAdder.mjs';
 export * from './Add16.mjs';
 export * from './Inc16.mjs';
 export * from './ALU.mjs';
+
 export * from './DFF.mjs';
 export * from './Bit.mjs';
 export * from './Register.mjs';
@@ -31,3 +33,6 @@ export * from './RAM512.mjs';
 export * from './RAM4K.mjs';
 export * from './RAM16K.mjs';
 export * from './PC.mjs';
+
+// // TODO: Figure out how to do the memory mapped peripherals
+// export * from './Memory.mjs';
