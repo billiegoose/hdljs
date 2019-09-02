d52cd394b3fffb8a9788bfbf8e9b150f9f49f40a
William Hilton <wmhilton@gmail.com>
2019-09-02 19:14:56 -0400

    sweep freq

--- src/lib/verilog/NOIZE.sv
+++ src/lib/verilog/NOIZE.sv
@@ -3,6 +3,7 @@ module NOIZE (
   input i_Clk,
   output o_Out
 );
+  reg [13:0] sweep = 14'b1;
   reg [15:0] snd_counter = 16'b0;
   reg r_Out = 1'b0;
   assign o_Out = r_Out;
@@ -10,9 +11,10 @@ module NOIZE (
   always @(negedge i_Clk) begin
     snd_counter <= snd_counter + 16'b1;
     // 25 000 000 / (2 * 440 Hz)
-    if (snd_counter == 28409) begin
+    if (snd_counter == sweep) begin
       r_Out <= ~r_Out;
       snd_counter <= 16'b0;
+      sweep <= sweep + 14'b1;
     end
   end
 endmodule
