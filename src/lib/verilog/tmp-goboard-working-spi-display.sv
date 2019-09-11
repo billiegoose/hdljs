8dd949b7e678fe0ac74cf96d3fa07d566c747e3c
William Hilton <wmhilton@gmail.com>
2019-09-10 20:47:39 -0400

    add UART spitting out '*' at 115200 baud

--- src/lib/verilog/tmp-goboard-working-spi-display.sv
+++ src/lib/verilog/tmp-goboard-working-spi-display.sv
@@ -5,6 +5,7 @@
 `include "./lib/SSD1306.v"
 
 `include "./lib/NOIZE.sv"
+`include "./lib/UART_Encoder.v"
 
 module Go_Board (
   input  i_Clk,
@@ -16,6 +17,8 @@ module Go_Board (
   output o_LED_2,
   output o_LED_3,
   output o_LED_4,
+  // input i_UART_RX,
+  output o_UART_TX,
   output o_Segment1_A,
   output o_Segment1_B,
   output o_Segment1_C,
@@ -39,6 +42,14 @@ module Go_Board (
   input io_PMOD_9,
   output io_PMOD_10,
 );
+
+  UART_Encoder UART_Encoder (
+    .i_Clk(i_Clk),
+    .i_Period(20'd217),
+    .i_Byte(8'h2A),
+    .i_write_enable(1'b1),
+    .o_UART_TX(o_UART_TX)
+  );
 
   wire w_Switch_1;
   Debouncer Debouncer_1 (
@@ -106,64 +117,6 @@ module Go_Board (
     .o_READY()
   );
 
-  // reg [15:0] address = 8'h0;
-  // wire [7:0] txByte;
-
-  // SPI_ROM SPI_ROM (
-  //   .address(address),
-  //   .out(txByte)
-  // );
-
-  // parameter SPI_MODE = 0; // CPOL = 0, CPHA = 0
-  // parameter CLKS_PER_HALF_BIT = 1;  // 6.25 MHz
-
-  // reg [7:0] r_Master_TX_Byte = 8'hFF;
-  // reg r_TX_DV = 1'b0;
-  // // logic [7:0] r_Master_RX_Byte;
-
-  // // Instantiate UUT
-  // SPI_Master #(.SPI_MODE(SPI_MODE), .CLKS_PER_HALF_BIT(CLKS_PER_HALF_BIT))
-  // SPI_Master_UUT
-  // (
-  //   // Control/Data Signals,
-  //   .i_Rst_L(~reset),     // FPGA Reset
-  //   .i_Clk(clock),         // FPGA Clock
-
-  //   // TX (MOSI) Signals
-  //   //  .i_TX_Byte(r_Master_TX_Byte),  // Byte to transmit on MOSI
-  //   .i_TX_Byte(txByte),  // Byte to transmit on MOSI
-  //   .i_TX_DV(r_TX_DV),          // Data Valid Pulse with i_TX_Byte
-  //   .o_TX_Ready(o_LED_1),          // Transmit Ready for Byte
-
-  //   // RX (MISO) Signals
-  //   //  .o_RX_DV(r_Master_RX_DV),       // Data Valid pulse (1 clock cycle)
-  //   //  .o_RX_Byte(r_Master_RX_Byte),   // Byte received on MISO
-
-  //   // SPI Interface
-  //   .o_SPI_MOSI(o_LED_2),
-  //   .i_SPI_MISO(1'b0),
-  //   .o_SPI_Clk(o_LED_4)
-  // );
-
-  // reg r_TX_DV_delayed = 1'b0;
-
-  // always @(negedge clock) begin
-  //   if (i_Switch_2) begin
-  //     address <= 0;
-  //     r_TX_DV <= 1'b0;
-  //   end else begin
-  //     if (o_LED_1 == 1'b1) begin
-  //       r_TX_DV <= 1'b1;
-  //     end else begin
-  //       r_TX_DV <= 1'b0;
-  //     end
-  //     r_TX_DV_delayed <= r_TX_DV;
-  //     if (r_TX_DV_delayed == 1'b1) begin
-  //       address <= address + 1;
-  //     end
-  //   end
-  // end
-
   DoubleDigitDisplay DoubleDigitDisplay_0 (
     .i_Byte(rxByte),
     .o_Segment1_A(o_Segment1_A),
@@ -186,6 +139,7 @@ module Go_Board (
   assign io_PMOD_10 = w_AudioOut;
 
   NOIZE NOIZE (
+    .i_Reset(reset),
     .i_Clk(i_Clk),
     .o_Out(w_AudioOut)
   );
